3afe44c934f413fafa31d6088e1f3d8f0c39cf2f
JING ZE CHONG <cjzchong1@gmail.com>
2022-04-02 22:32:24 +0800

    feat: add country link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+import Plot from 'react-plotly.js';
+import {BrowserRouter as Router, Route, Routes,useParams } from 'react-router-dom'
+import {Header} from './components/Header.jsx'
+import Home from './components/Home'
+import PieCart from './components/cart/PieCart'
+import Countries from './components/region/Countries'
+import CountryInfo from './components/region/CountryInfo'
+
+import {ErrorPage} from './components/ErrorPage'
+import { Component } from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Router>
+      <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/cart/piecart" element={<PieCart/>} />
+          <Route exact path="/countries" element={<Countries/>} />
+          {/* <Route path="country/:id" render={(props) => <CountryInfo {...props} />} /> */}
+          <Route path="/country/:id" element={<CountryInfo/>} />
+          {/* <Route path="/country/:id">
+            <CountryInfo />
+          </Route> */}
+          {/* <Route path="/signup" component={SignUp}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/addproduct" component={AddProduct}/> 
+          <PrivateRoute path="/cart" component={Cart}/> */}
+          {/* <Route element={ErrorPage}/> */}
+          <Route path="*" element={<ErrorPage/>}/>
+      </Routes>
+      </Router>
+    </>
   );
 }
 
