55afad35f8a5188c82015849c24e803b6aad4fae
JING ZE CHONG <cjzchong1@gmail.com>
2022-04-05 01:01:39 +0800

    refactor: comment unuse code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Plot from 'react-plotly.js';
 import {BrowserRouter as Router, Route, Routes,useParams } from 'react-router-dom'
 import {Header} from './components/Header.jsx'
 import Home from './components/Home'
-import PieCart from './components/cart/PieCart'
+// import PieChart from './components/cart/PieChart'
 import Countries from './components/region/Countries'
 import CountryInfo from './components/region/CountryInfo'
 
@@ -19,7 +19,7 @@ function App() {
       <Router>
       <Routes>
           <Route exact path="/" element={<Home/>} />
-          <Route exact path="/cart/piecart" element={<PieCart/>} />
+          {/* <Route exact path="/cart/piecart" element={<PieCart/>} /> */}
           <Route exact path="/countries" element={<Countries/>} />
           {/* <Route path="country/:id" render={(props) => <CountryInfo {...props} />} /> */}
           <Route path="/country/:id" element={<CountryInfo/>} />
