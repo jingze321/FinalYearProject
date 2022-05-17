7ccd09abda7461b71514426a962cf0e1dc5d2c1e
JING ZE CHONG <cjzchong1@gmail.com>
2022-05-17 14:02:55 +0800

    fix(data): fix data display wrong value

--- src/components/region/CountryInfo.jsx
+++ src/components/region/CountryInfo.jsx
@@ -83,43 +83,53 @@ function CountryInfo() {
           // console.log(countryRegion,'countryRegion');
           Object.keys(countryRegion)
             .map((state,index) =>{
-
-            Object.values(countryRegion[state]).map((stateValue,index)=>{
-                
-              if (!analyzeData[state]){
+              const currentStateInfo = countryRegion[state][countryRegion[state].length-1];
                 analyzeData[state] = {
-                  confirmed : 0,
-                  recovered : 0,
-                  death : 0,
-                  lat:parseFloat(stateValue.Lat),
-                  lon:parseFloat(stateValue.Lon),
+                  confirmed : currentStateInfo.Confirmed,
+                  recovered : currentStateInfo.Recovered,
+                  death : currentStateInfo.Deaths,
+                  lat:parseFloat(currentStateInfo.Lat),
+                  lon:parseFloat(currentStateInfo.Lon),
                   symbol:state==="All"?'square':'circle',
                   color:state==="All"?'#bebada':'#fdb462'
                 }
-              }else{
-                analyzeData[state]['confirmed'] += stateValue.Confirmed
-                analyzeData[state]['recovered'] += stateValue.Recovered
-                analyzeData[state]['death'] += stateValue.Deaths
+
+            // Object.values(countryRegion[state]).map((stateValue,index)=>{
                 
-              }
+            //   if (!analyzeData[state]){
+            //     analyzeData[state] = {
+            //       confirmed : 0,
+            //       recovered : 0,
+            //       death : 0,
+            //       lat:parseFloat(stateValue.Lat),
+            //       lon:parseFloat(stateValue.Lon),
+            //       symbol:state==="All"?'square':'circle',
+            //       color:state==="All"?'#bebada':'#fdb462'
+            //     }
+            //   }else{
+            //     analyzeData[state]['confirmed'] += stateValue.Confirmed
+            //     analyzeData[state]['recovered'] += stateValue.Recovered
+            //     analyzeData[state]['death'] += stateValue.Deaths
+                
+            //   }
             
-            });
+            // });
           });
 
-
+          //Calculate Object All if not exist from API
           const CountryCode = Object.values(countryRegion).length>0?Object.values(countryRegion)[0][0]['CountryCode']:null;
           if (!CountryCode) return;
           axios.get(`https://restcountries.com/v3.1/alpha/${CountryCode}`)
           .then(res => {
             if (res.data.length >0){
               setAnalyzeData({...analyzeData,'AllCountryInfo':res.data[0]})
-              console.log('AllCountryInfo',res.data[0]);
+              const countryGeoInfo = Object.values(res.data[0]);
+              console.log(countryGeoInfo,'countryGeoInfo');
               setISORegion(res.data[0].cca3)
               if (!analyzeData['All']){
                 let totalConfirmed =0;
                 let totalRecovered =0;
                 let totalDeath =0;
-
                 Object.values(analyzeData).map(x=>{
                    totalConfirmed += x.confirmed;
                    totalRecovered += x.recovered;
@@ -166,13 +176,13 @@ function CountryInfo() {
           let totalConfirmed =0;
           let totalRecovered =0;
           let totalDeath =0;
-
           Object.values(analyzeData).map((values)=>{
             // if (x.confirmed) {
               totalConfirmed += values.confirmed??0;
               totalRecovered += values.recovered??0;
               totalDeath += values.death??0;
           })
+
           setAnalyzeData({...analyzeData,
             'All':{
               confirmed : totalConfirmed,
@@ -224,10 +234,10 @@ function CountryInfo() {
                             </div>
                             <div className="media-body align-self-center mx-1">
                               <h4>Population</h4>
-                              <span>Monthly blog comments</span>
+                              <span>Total Population</span>
                             </div>
                             <div className="align-self-center ms-auto p-1 ">
-                              <h1>{analyzeData.AllCountryInfo.population}</h1>
+                              <h2>{analyzeData.AllCountryInfo.population}</h2>
                             </div>
                           </div>
                           {summaryInfo&&
@@ -251,13 +261,20 @@ function CountryInfo() {
                             <FontAwesomeIcon icon={faKitMedical}  className="fa-4x" style={{  color: "green" }}/>
                             </div>
                             <div className="media-body align-self-center mx-1">
-                              <h4>Population</h4>
-                              <span>Country Total Population</span>
+                              <h4>Size </h4>
+                              <span>Country area </span>
                             </div>
                             <div className="align-self-center ms-auto p-1 ">
-                              <h1>{analyzeData.AllCountryInfo.population}</h1>
+                              <h2>{(analyzeData.AllCountryInfo.area)}
+                              <small style={{"font-size":"0.8rem"}}className="text-muted">KM &#xB2;</small>
+                              </h2>
+                              
                             </div>
                           </div>
+                          <ProgressBar variant="success">
+                              <ProgressBar striped variant="secondary" now={(analyzeData.AllCountryInfo.area/5101000)} key={1} />
+                              {/* <ProgressBar striped variant="danger" now={(summaryInfo['TotalRecovered']/analyzeData.AllCountryInfo.population)*100} key={3} /> */}
+                          </ProgressBar>
                         </div>
                       </div>
                     </div>
@@ -273,9 +290,12 @@ function CountryInfo() {
               <span> Chart Type:</span>
               <Select
                 className=""
-                value={selectedChartModel}//{ label: "All", value: "All" }
+                value={selectedChartModel}
                 onChange={setSelectedChartModel}
-                options={[{value:'PieChart',label:'PieChart'},{value:'BarChart',label:'BarChart'},{value:'LineChart',label:'LineChart'}]}
+                options={[
+                  {value:'PieChart',label:'PieChart'},
+                  {value:'BarChart',label:'BarChart'},
+                  {value:'LineChart',label:'LineChart'}]}
               />
             </div>
             <hr/>
